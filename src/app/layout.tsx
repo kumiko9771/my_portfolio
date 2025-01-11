24809660a915d90ba0b1c2649651d18db02fff53
Kumiko Shimizu <kumiko@KumikonoMacBook-5.local>
2025-01-11 18:38:54 +0900

    add navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>navigation</div>
         {children}
       </body>
     </html>
