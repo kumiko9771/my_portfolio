29adffff6beeabaa95724c5f3e9feadb8a784473
zhiye-tomoya <142067681+zhiye-tomoya@users.noreply.github.com>
2025-01-15 21:13:45 +0900

    define NavItemProps in BottomNav.tsx

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { ListTodo, UserRoundPen } from "lucide-react";
+import { LucideIcon, ListTodo, UserRoundPen } from "lucide-react";
 import Link from "next/link";
 
 const navs = [
@@ -9,7 +9,7 @@ const navs = [
   },
   {
     href: "/me",
-    icon: ListTodo,
+    icon: UserRoundPen,
     name: "マイページ",
   },
 ];
@@ -18,13 +18,24 @@ export const BottomNav = () => {
   return (
     <div className='fixed bottom-0 left-0 w-full flex md:hidden justify-around bg-white py-2 border-t'>
       {navs.map(({ href, icon: Icon, name }, i) => {
-        return (
-          <Link href={href} className='flex flex-col items-center'>
-            <Icon size={28} />
-            <h1 className='text-xs'>{name}</h1>
-          </Link>
-        );
+        return <NavItem key={i} href={href} icon={Icon} name={name} />;
       })}
     </div>
+  );
+};
+
+interface NavItemProps {
+  key: number;
+  href: string;
+  icon: LucideIcon;
+  name: string;
+}
+
+const NavItem = ({ key, href, icon: Icon, name }: NavItemProps) => {
+  return (
+    <Link key={key} href={href} className='flex flex-col items-center'>
+      <Icon size={28} />
+      <h1 className='text-xs'>{name}</h1>
+    </Link>
   );
 };
