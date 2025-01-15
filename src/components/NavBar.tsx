9762cf5769fdbf55386f4cceffee941982323176
zhiye-tomoya <142067681+zhiye-tomoya@users.noreply.github.com>
2025-01-15 19:51:43 +0900

    hide NavBar on mobile screen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,15 @@
 const navNames = ["top", "about", "comunity", "contact"];
 
-
-
 export const Navbar = () => {
-    return(
-        <div className="flex justify-between p-4 border-b">
-            {
-                navNames.map((name, i) => {
-                    return(
-                        <div key={i} className="font-semibold hover:text-blue-500">{name}</div>
-                    )
-                })
-            }
-        </div>
-    )
-} 
+  return (
+    <div className='hidden md:flex justify-between p-4 border-b'>
+      {navNames.map((name, i) => {
+        return (
+          <div key={i} className='font-semibold hover:text-blue-500'>
+            {name}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
